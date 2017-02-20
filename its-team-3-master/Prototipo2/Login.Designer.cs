b9e2f4fddade82f6280a8085c2ed532a427da165
zerocinque <gigi.bottasso@gmail.com>
2017-02-20 13:01:31 +0100

    Add files via upload
    reso più leggibile codice

---
+++ its-team-3-master/Prototipo2/Login.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Prototipo2
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this._login = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this._exit = new System.Windows.Forms.Button();
+            this._registrazione = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(211, 141);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(254, 20);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(211, 177);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(254, 20);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(96, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Password:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Lime;
+            this.label4.Location = new System.Drawing.Point(96, 144);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "E-mail :";
+            // 
+            // _login
+            // 
+            this._login.BackColor = System.Drawing.Color.Yellow;
+            this._login.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._login.Location = new System.Drawing.Point(241, 225);
+            this._login.Name = "_login";
+            this._login.Size = new System.Drawing.Size(141, 55);
+            this._login.TabIndex = 12;
+            this._login.Text = "LOGIN";
+            this._login.UseVisualStyleBackColor = false;
+            this._login.Click += new System.EventHandler(this._login_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(258, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 38);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Login";
+            // 
+            // _exit
+            // 
+            this._exit.BackColor = System.Drawing.Color.Yellow;
+            this._exit.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._exit.Location = new System.Drawing.Point(502, 262);
+            this._exit.Name = "_exit";
+            this._exit.Size = new System.Drawing.Size(98, 44);
+            this._exit.TabIndex = 14;
+            this._exit.Text = "EXIT";
+            this._exit.UseVisualStyleBackColor = false;
+            this._exit.Click += new System.EventHandler(this._exit_Click);
+            // 
+            // _registrazione
+            // 
+            this._registrazione.BackColor = System.Drawing.Color.Yellow;
+            this._registrazione.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this._registrazione.Location = new System.Drawing.Point(12, 262);
+            this._registrazione.Name = "_registrazione";
+            this._registrazione.Size = new System.Drawing.Size(105, 44);
+            this._registrazione.TabIndex = 15;
+            this._registrazione.Text = "REGISTRATI";
+            this._registrazione.UseVisualStyleBackColor = false;
+            this._registrazione.Click += new System.EventHandler(this._registrazione_Click);
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(612, 318);
+            this.Controls.Add(this._registrazione);
+            this.Controls.Add(this._exit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this._login);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.login_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button _login;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button _exit;
+        private System.Windows.Forms.Button _registrazione;
+    }
+}
---
+++ its-team-3-master/Prototipo2/Registrazione.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace Prototipo2
+{
+    public partial class Registrazione : Form
+    {
+        public Registrazione()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) { }
+
+        private void label5_Click(object sender, EventArgs e) { }
+
+        private void label4_Click(object sender, EventArgs e) { }
+
+        private void label3_Click(object sender, EventArgs e) { }
+
+        private void label2_Click(object sender, EventArgs e) { }
+
+        private void label1_Click(object sender, EventArgs e) { }
+
+        private void invia_Click(object sender, EventArgs e)
+        {
+            if (textBox2.TextLength>4)
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Utente\Desktop\its-team-3-master\Database\tony.mdf;Integrated Security=True;Connect Timeout=30");
+                string str = "insert into [Table] (nome,cognome,email,password) values ('" + textBox1.Text + "','" + textBox4.Text + "','" + textBox3.Text + "','" + textBox2.Text + "')";
+                con.Open();
+                SqlCommand cmd = new SqlCommand(str, con);
+                cmd.ExecuteNonQuery();
+                con.Close();
+                MessageBox.Show("Registrazione riuscita");
+                Login lg = new Login();
+                this.Hide();
+                lg.Show();
+                
+            }
+            else
+            {
+                MessageBox.Show("Password inserita inferiore ad 5 caratteri");
+            }
+        }
+    }
+}
