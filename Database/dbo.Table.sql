25bc7c770a77d9fc0c20b043ec773555aae4e454
zerocinque <gigi.bottasso@gmail.com>
2017-02-22 14:12:38 +0100

    Delete dbo.Table.sql

--- Database/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[email] VARCHAR(50) NOT NULL , 
-    [password] VARCHAR(50) NOT NULL, 
-    [nome] VARCHAR(50) NOT NULL, 
-    [cognome] VARCHAR(50) NULL, 
-    PRIMARY KEY ([password])
-)
