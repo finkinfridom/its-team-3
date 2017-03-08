e5f2eaf1bf8f43c78604a014e0500fa83077f322
zerocinque <gigi.bottasso@gmail.com>
2017-03-08 11:00:19 +0100

    Add files via upload
    design + codice lunedi

--- classbooking/classbooking/Registrazione.Designer.cs
+++ classbooking/classbooking/Registrazione.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registrazione));
             this.esci = new System.Windows.Forms.Button();
             this.invia = new System.Windows.Forms.Button();
             this.insertCognome = new System.Windows.Forms.TextBox();
@@ -43,108 +44,125 @@
             // 
             // esci
             // 
-            this.esci.BackColor = System.Drawing.Color.Red;
-            this.esci.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.esci.Location = new System.Drawing.Point(274, 219);
+            this.esci.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.esci.BackColor = System.Drawing.Color.Snow;
+            this.esci.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.esci.Location = new System.Drawing.Point(335, 219);
             this.esci.Name = "esci";
             this.esci.Size = new System.Drawing.Size(115, 42);
-            this.esci.TabIndex = 22;
+            this.esci.TabIndex = 6;
             this.esci.Text = "EXIT";
             this.esci.UseVisualStyleBackColor = false;
             this.esci.Click += new System.EventHandler(this.esci_Click);
             // 
             // invia
             // 
-            this.invia.BackColor = System.Drawing.Color.Yellow;
-            this.invia.Font = new System.Drawing.Font("Impact", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.invia.Location = new System.Drawing.Point(153, 219);
+            this.invia.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.invia.BackColor = System.Drawing.Color.Snow;
+            this.invia.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.invia.Location = new System.Drawing.Point(196, 219);
             this.invia.Name = "invia";
             this.invia.Size = new System.Drawing.Size(115, 42);
-            this.invia.TabIndex = 21;
-            this.invia.Text = "REGISTRA";
+            this.invia.TabIndex = 5;
+            this.invia.Text = "REGISTRATI";
             this.invia.UseVisualStyleBackColor = false;
             this.invia.Click += new System.EventHandler(this.invia_Click);
             // 
             // insertCognome
             // 
+            this.insertCognome.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.insertCognome.Location = new System.Drawing.Point(196, 98);
             this.insertCognome.Name = "insertCognome";
             this.insertCognome.Size = new System.Drawing.Size(254, 20);
-            this.insertCognome.TabIndex = 20;
+            this.insertCognome.TabIndex = 2;
             // 
             // insertEmail
             // 
+            this.insertEmail.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.insertEmail.Location = new System.Drawing.Point(196, 129);
             this.insertEmail.Name = "insertEmail";
             this.insertEmail.Size = new System.Drawing.Size(254, 20);
-            this.insertEmail.TabIndex = 19;
+            this.insertEmail.TabIndex = 3;
             // 
             // insertPassword
             // 
+            this.insertPassword.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.insertPassword.Location = new System.Drawing.Point(196, 165);
             this.insertPassword.Name = "insertPassword";
             this.insertPassword.Size = new System.Drawing.Size(254, 20);
-            this.insertPassword.TabIndex = 18;
+            this.insertPassword.TabIndex = 4;
+            this.insertPassword.TextChanged += new System.EventHandler(this.insertPassword_TextChanged);
             // 
             // insertNome
             // 
+            this.insertNome.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.insertNome.Location = new System.Drawing.Point(196, 66);
             this.insertNome.Name = "insertNome";
             this.insertNome.Size = new System.Drawing.Size(254, 20);
-            this.insertNome.TabIndex = 17;
+            this.insertNome.TabIndex = 1;
             // 
             // label5
             // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.Lime;
-            this.label5.Location = new System.Drawing.Point(81, 165);
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Snow;
+            this.label5.Location = new System.Drawing.Point(81, 163);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(60, 16);
+            this.label5.Size = new System.Drawing.Size(91, 20);
             this.label5.TabIndex = 16;
             this.label5.Text = "Password:";
             // 
             // label4
             // 
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Lime;
-            this.label4.Location = new System.Drawing.Point(81, 132);
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Snow;
+            this.label4.Location = new System.Drawing.Point(81, 127);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(53, 16);
+            this.label4.Size = new System.Drawing.Size(69, 20);
             this.label4.TabIndex = 15;
             this.label4.Text = "E-mail :";
             // 
             // label3
             // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Lime;
-            this.label3.Location = new System.Drawing.Point(81, 101);
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Snow;
+            this.label3.Location = new System.Drawing.Point(81, 96);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 16);
+            this.label3.Size = new System.Drawing.Size(95, 20);
             this.label3.TabIndex = 14;
             this.label3.Text = "Cognome :";
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.Lime;
-            this.label2.Location = new System.Drawing.Point(81, 70);
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Snow;
+            this.label2.Location = new System.Drawing.Point(81, 64);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 16);
+            this.label2.Size = new System.Drawing.Size(65, 20);
             this.label2.TabIndex = 13;
             this.label2.Text = "Nome :";
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(191, 4);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Snow;
+            this.label1.Location = new System.Drawing.Point(226, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(143, 29);
+            this.label1.Size = new System.Drawing.Size(194, 31);
             this.label1.TabIndex = 12;
             this.label1.Text = "Registrazione";
             // 
@@ -152,7 +170,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(560, 327);
             this.Controls.Add(this.esci);
             this.Controls.Add(this.invia);
@@ -167,6 +188,8 @@
             this.Controls.Add(this.label1);
             this.Name = "Registrazione";
             this.Text = "Registrazione";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Registrazione_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
