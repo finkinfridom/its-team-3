53a41e250f6d73c3b32408d34033b935fdf77e06
francescotalomo <francescotalomo@edu.itspiemonte.it>
2017-03-15 16:44:43 +0100

    sfondo e impaginazione ultima pagina

--- classbooking/classbooking/Registrazione_effettuata.Designer.cs
+++ classbooking/classbooking/Registrazione_effettuata.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Registrazione_effettuata));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textemail = new System.Windows.Forms.TextBox();
@@ -35,25 +36,33 @@
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(82, 18);
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(81, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(76, 25);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.button1.BackColor = System.Drawing.Color.Snow;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(85, 101);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(89, 87);
+            this.button1.Size = new System.Drawing.Size(100, 87);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Text = "INVIA";
+            this.button1.UseVisualStyleBackColor = false;
             // 
             // textemail
             // 
-            this.textemail.Location = new System.Drawing.Point(85, 64);
+            this.textemail.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.textemail.Location = new System.Drawing.Point(85, 52);
             this.textemail.Name = "textemail";
             this.textemail.Size = new System.Drawing.Size(100, 20);
             this.textemail.TabIndex = 2;
@@ -62,12 +71,15 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.textemail);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Registrazione_effettuata";
             this.Text = "Registrazione_effettuata";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
             this.PerformLayout();
 
